95db2e3754710fef2dff1f67cd269d7c04513973
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-03-29 10:58:29 -0500

    infiniti scroll finished

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,51 @@
-import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import React, {Component} from 'react';
+import {carouselData} from '../../data';
+
 export default class Carousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: [],
+            index: 0
+        }
     }
-  }
-  componentDidMount(){
+    componentDidMount() {
+        this.setState({images: carouselData})
+    }
 
-  }
+    leftClick = () => {
+        let nextIndex;
+        if (this.state.index === 0) 
+            nextIndex = this.state.images.length - 1;
+        else 
+            nextIndex = this.state.index - 1;
+        this.setState({index: nextIndex});
+    }
 
-  leftClick = () => {
+    rightClick = () => {
+        let nextIndex;
+        if (this.state.index === this.state.images.length - 1) 
+            nextIndex = 0;
+        else 
+            nextIndex = this.state.index + 1;
+        this.setState({index: nextIndex});
+    }
 
-  }
+    selectedImage = () => {
+        return <img
+            src={this.state.images[this.state.index]}
+            style={{
+            display: 'block'
+        }}/>
+    }
 
-  rightClick = () => {
-
-  }
-
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
-    return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="carousel">
+                <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+                <div className="right-button" onClick={this.rightClick}>{">"}</div>
+                {this.selectedImage()}
+            </div>
+        )
+    }
 }
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -59,6 +60,7 @@ export default class Content extends Component {
         <Tabs tabs={this.state.tabs}
           selectedTab={this.state.selected}
           selectedTabHandler={this.selectedTabHandler}/>
+        <Carousel/>
         <Cards cards={this.filterCards()} />
       </div>
     );
